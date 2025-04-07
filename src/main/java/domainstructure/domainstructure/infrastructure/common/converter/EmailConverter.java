93e6b7d66321ac465b8f4596d4647e17dbf81be7
JRJR519 <kkwjdfo@gmail.com>
2025-04-07 23:38:50 +0900

    feat(Email): jpa 변환 컨버터 추가

---
+++ src/main/java/domainstructure/domainstructure/infrastructure/common/converter/EmailConverter.java
@@ -0,0 +1,17 @@
+package domainstructure.domainstructure.infrastructure.common.converter;
+
+import domainstructure.domainstructure.core.common.Email;
+import jakarta.persistence.AttributeConverter;
+
+public class EmailConverter implements AttributeConverter<Email, String> {
+    @Override
+    public String convertToDatabaseColumn(Email attribute) {
+        return attribute == null ? null : attribute.getValue();
+    }
+
+    @Override
+    public Email convertToEntityAttribute(String dbData) {
+        return dbData == null ? null : Email.of(dbData);
+    }
+
+}
